5cce77512975c38ee305c0fff7f51c1007660051
Yoram Hekma <yhekma@users.noreply.github.com>
2016-04-06 15:06:41 +0200

    Update restore_backup.py
    Final update for 1.9x

--- library/restore_backup.py
+++ library/restore_backup.py
@@ -1,19 +1,35 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 import os
 import re
-import json
-import shutil
+
+# -*- coding: utf-8 -*-
+
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
 DOCUMENTATION = '''
 ---
 module: restore_backup
-short_description: restores a backup created with 'backup=true' using, for instance, the lineinfile module
+short_description: Restores a backup created with 'backup=true' using, for instance, the lineinfile module
 description:
   - looks for a backup file in the same dir as the file to be restored and renames it to the original.
-author: Yoram Hekma
+author: "Yoram Hekma (@yhekma)"
+version_added: "2.0"
 requirements:
-  - python > 2.6
+  - python > 2.4
 options:
   dest:
     description:
@@ -26,45 +42,61 @@ options:
     required: false
     choices: ["yes", "no"]
     default: "no"
+  version:
+    description:
+      - The version of the backup to restore. Can either be "latest" or "earliest". When only 1 backup exists it comes down to the same thing.
+    required: false
+    choices: ["latest", "earliest"]
+    default: "latest"
 '''
 
 EXAMPLES = '''
-  - action: restore_backup dest=/etc/samba/smb.conf
+# Restore a backup of /etc/samba/smb.conf, created with, for instance, lineinfile. Prior to restoring, back up the current version
+  - name: restore previous state of smb.conf
+    restore_backup: dest=/etc/samba/smb.conf backup=yes
+
+# Restore the first made backup of /etc/ntp.conf without making a backup first
+  - name: restore first state of /etc/ntp.conf
+    restore_backup: dest=/etc/ntp.conf version=earliest
 '''
 
 
-def restore_backup(dest):
+def restore_backup(module, dest, version, original=None):
     dirpath = os.path.dirname(dest)
     filename = os.path.basename(dest)
     file_dict = dict()
+    time_regexp = re.compile('^%s\.(\d{4}(:?-\d\d){2}@(:?\d\d:){2}\d\d~$)' % filename)
 
     for f in os.listdir(dirpath):
         # This will match: "<filename>.2015-05-26@11:50:47~" for instance
-        if re.match('^%s\.\d{4}(:?-\d\d){2}@(:?\d\d:){2}\d\d~$' % filename, f):
-            mtime = os.stat(os.path.join(dirpath, f)).st_mtime
-            file_dict[str(mtime)] = f
+        try:
+            if f == os.path.basename(original):
+                continue
+            timestring = re.match(time_regexp, f).group(1)
+            mtime = time.strptime(timestring, '%Y-%m-%d@%H:%M:%S~')
+            file_dict[mtime] = f
+        except AttributeError:
+            continue
 
-    mtime_list = [float(i) for i in file_dict.keys()]
+    mtime_list = [i for i in file_dict.keys()]
     mtime_list.sort()
     try:
-        latest_backup = file_dict[str(mtime_list[-1])]
-    except (IndexError, KeyError):
-        return {
-            'changed': False,
-            'stderr': 'No backup found',
-        }
+        if version == 'latest':
+            backup_file = file_dict[mtime_list[-1]]
+        else:
+            backup_file = file_dict[mtime_list[0]]
 
-    backup_path = os.path.join(dirpath, latest_backup)
+    except (IndexError, KeyError):
+        module.exit_json(stderr='No backup found', changed=False)
+
+    backup_path = os.path.join(dirpath, backup_file)
 
     try:
-        os.rename(backup_path, dest)
+        module.atomic_move(backup_path, dest)
     except Exception, e:
-        return {
-            'changed': False,
-            'stderr': json.dumps("%s (renaming %s to %s)" % (e, backup_path, dest)),
-        }
+        module.fail_json(stderr="%s (renaming %s to %s)" % (e, backup_path, dest), changed=False)
 
-    return {'changed': True}
+    return dict(changed=True, stderr=str())
 
 
 def main():
@@ -72,31 +104,22 @@ def main():
         argument_spec=dict(
             dest=dict(required=True),
             backup=dict(default=False, type='bool'),
+            version=dict(default='latest', choices=['latest', 'earliest']),
         )
     )
 
     dest = module.params['dest']
+    version = module.params['version']
 
     if module.params['backup']:
-        ext = time.strftime("%Y-%m-%d@%H:%M:%S~", time.localtime(time.time()))
-        backup_dest = "%s.%s-tmp" % (dest, ext)
-        try:
-            shutil.copy2(dest, backup_dest)
-        except (shutil.Error, IOError), e:
-            module.fail_json(msg='Could not make backup of %s to %s: %s' % (dest, backup_dest, e))
+        original = module.backup_local(dest)
 
-        res_args = restore_backup(dest)
-
-        try:
-            os.rename(backup_dest, '%s.%s' % (dest, ext))
-        except Exception, e:
-            module.fail_json(msg='Could not rename %s to %s.%s: %s' % (backup_dest, dest, ext, e))
+        res_args = restore_backup(module, dest, version, original)
 
     else:
-        res_args = restore_backup(dest)
+        res_args = restore_backup(module, dest, version)
 
     module.exit_json(**res_args)
-
 
 from ansible.module_utils.basic import *
 
